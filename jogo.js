e53af7e11f59027ba937a2a77bcd3d0a8cc76cd4
Rafaela Miranda <rafaela-gomes@outlook.com>
2018-12-06 10:34:27 -0200

    Finalização de jogo

--- jogo.js
+++ jogo.js
@@ -1,3 +1,5 @@
+var timerId = null; //variável que armazena a chamada da função timeOut
+
 function iniciaJogo(){
     var url = window.location.search;
     var nivel_jogo = url.replace("?", "");
@@ -21,14 +23,35 @@ function iniciaJogo(){
     document.getElementById('cronometro').innerHTML = tempo_segundos;
 
     // quantidade de balões
-    var qtde_baloes = 20;
+    var qtde_baloes = 80;
 
     cria_baloes(qtde_baloes);
 
     //imprimir quantidade de balões inteiros
     document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+
+    //imprimir quantidade de balões estourados
     document.getElementById('baloes_estourados').innerHTML = 0;
 
+    contagem_tempo(tempo_segundos + 1);
+
+}
+
+function contagem_tempo(segundos){
+    segundos = segundos - 1;
+
+    if(segundos == -1){
+        clearTimeout(timerId); //para a execução da função do setTimeOut
+        game_over();
+        return false;
+    }
+
+    document.getElementById('cronometro').innerHTML = segundos;
+    timerId = setTimeout("contagem_tempo("+segundos+")", 1000);
+}
+
+function game_over(){
+    alert('Fim de jogo, você não conseguiu estourar todos os balões a tempo');
 }
 
 function cria_baloes(qtde_baloes){
@@ -36,9 +59,44 @@ function cria_baloes(qtde_baloes){
         var balao = document.createElement("img");
         balao.src = 'imagens/balao_azul_pequeno.png';
         balao.style.margin = '10px';
+        balao.id = 'b'+i;
+        balao.onclick = function() { estourar(this); }
 
         document.getElementById('jogo').appendChild(balao);
 
     }
+}
+
+function estourar(e){
+    var id_balao = e.id;
+    document.getElementById(id_balao).src = 'imagens/balao_azul_pequeno_estourado.png';
+    pontuacao(-1);
+}
+
+function pontuacao(acao){
+    var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+    var baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
+    
+    baloes_inteiros = parseInt(baloes_inteiros);
+    baloes_estourados = parseInt(baloes_estourados);
+
+    baloes_inteiros = baloes_inteiros + acao;
+    baloes_estourados = baloes_estourados - acao;
+
+    document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
+    document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
+
+    situacao_jogo(baloes_inteiros);
+}
+
+function situacao_jogo(baloes_inteiros){
+    if(baloes_inteiros == 0){
+        alert('Parabéns, você conseguiu estourar todos os balões e tempo');
+        parar_jogo();
+    }
+}
+
+function parar_jogo(){
+    clearInterval(timerId);
 }
 
